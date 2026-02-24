a320fdadd3d001bce30943d3dee4a5b633b610ec
Astha <aasthasingh2317@email.com>
2026-02-24 16:57:18 +0530

    first commit

---
+++ portfolio/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+    level = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    link = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Contact(models.Model):   # 👈 SAME LEVEL
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.full_name
---
+++ portfolio/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail
+
+def home(request):
+    form = ContactForm()
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data["name"]
+            email = form.cleaned_data["email"]
+            message = form.cleaned_data["message"]
+
+            send_mail(
+                f"Message from {name}",
+                message,
+                email,
+                ["asthasingh2317@gmail.com"],  # your email
+            )
+
+    return render(request, "portfolio/home.html")
